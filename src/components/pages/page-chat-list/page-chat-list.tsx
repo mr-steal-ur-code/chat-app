15984a41042597bcb35eec2973e8e1f6962efa3f
mr-steal-ur-code <cjkarkut@gmail.com>
2023-06-29 17:06:56 -0500

    update dependecies and service worker

--- src/components/pages/page-chat-list/page-chat-list.tsx
+++ src/components/pages/page-chat-list/page-chat-list.tsx
@@ -8,14 +8,7 @@ export class PageChatList {
   @Event() forceUpdate: EventEmitter;
   render() {
     return <div>
-      <ion-button
-                title="Check for updates"
-                slot="start"
-                fill="clear"
-                onClick={() => this.forceUpdate.emit({})}
-              >
-                <ion-icon color="primary" slot="icon-only" name="refresh-circle"/>
-              </ion-button>
+      <h1>This page is blank 🤷‍♂️</h1>
     </div>;
   }
 }
--- src/sw.js
+++ src/sw.js
@@ -2,10 +2,27 @@
 importScripts('workbox-v4.3.1/workbox-sw.js');
 
 // your custom service worker code
-self.addEventListener("message", ({ data }) => {
-  if (data === "skipWaiting") {
-    self.skipWaiting();
+self.addEventListener("message", (event,{ data }) => {
+  if (event.request.mode === 'navigate' && registration.waiting) {
+    if (( clients.matchAll()).length < 2) {
+      registration.waiting.postMessage("skipWaiting");
+    }
   }
+});
+
+self.addEventListener("fetch",event => {
+  event.respondWith((async () => {
+  if (event.request.mode === "navigate" &&
+    event.request.method === "GET" &&
+    registration.waiting &&
+    (await clients.matchAll()).length < 2
+  ) {
+    registration.waiting.postMessage('skipWaiting');
+    return new Response("", {headers: {"Refresh": "0"}});
+  }
+  return await caches.match(event.request) ||
+    fetch(event.request);
+})());
 });
 
 // the precache manifest will be injected into the following line
